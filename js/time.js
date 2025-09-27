adc0fb3de50af6b44d53bb7b999f7010fb778547
dachun <31814239+lidachun8@users.noreply.github.com>
2025-09-27 20:42:04 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,6 +1,6 @@
 ﻿function showTime() { 
 	var currentDate = new Date(); 
-	var startDate = new Date(2025,03,31);
+	var startDate = new Date(2025,03,30);
 	var date3 = currentDate-startDate;
 	var days=Math.floor(date3/(24*3600*1000));
 	var leave1=date3%(24*3600*1000);     //计算天数后剩余的毫秒数
